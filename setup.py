19b6ea9e2724c9ea045ff4b570ec3099fc15c6dc
xadrianzetx <xadrianzetx@gmail.com>
2022-08-02 19:31:20 +0200

    Make build system PEP 517 spec

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 import os
+from typing import Any
 
-import numpy as np
 from setuptools import find_packages
 from setuptools import setup
-from Cython.Build import cythonize
 
 
 def get_version() -> str:
@@ -17,13 +16,27 @@ def get_version() -> str:
             raise RuntimeError("Didn't manage to find version string.")
 
 
+def _cythonize(*args: Any, **kwargs: Any) -> Any:
+
+    from Cython.Build import cythonize
+
+    return cythonize(*args, **kwargs)
+
+
+def _numpy_get_include() -> str:
+
+    import numpy as np
+
+    return np.get_include()
+
+
 setup(
     name="fastcontour",
     version=get_version(),
     author="Adrian Zuber",
     url="https://github.com/xadrianzetx/optuna-fastcontour",
     packages=find_packages(exclude=("tests")),
-    ext_modules=cythonize([os.path.join("fastcontour", ext) for ext in ["*.pyx", "*.pxd"]]),
+    ext_modules=_cythonize([os.path.join("fastcontour", ext) for ext in ["*.pyx", "*.pxd"]]),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
@@ -34,5 +47,5 @@ setup(
     python_requires=">=3.7",
     install_requires=["optuna @ git+https://github.com/optuna/optuna.git", "matplotlib"],
     extras_require={"test": ["pytest"]},
-    include_dirs=[np.get_include()],
+    include_dirs=[_numpy_get_include()],
 )
